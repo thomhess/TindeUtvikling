a88d841ccfc1e77f261c6008873b3bd611ef2b82
Thomas Hesselberg <hessthom@gmail.com>
2017-03-23 11:40:12 +0100

    Map on index

---
+++ Prototype/js/index-map.js
@@ -0,0 +1,18 @@
+function initMap() {
+        var venabygd = {lat: 61.668, lng: 10.240};
+        var gala = {lat: 61.517, lng: 9.777}
+        
+        var map = new google.maps.Map(document.getElementById('googlemap'), {
+          zoom: 8,
+          center: venabygd
+        });
+        var marker = new google.maps.Marker({
+          position: venabygd,
+          map: map
+        });
+    
+        var marker2 = new google.maps.Marker({
+          position: gala,
+          map: map
+        });
+      }
