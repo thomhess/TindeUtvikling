11b0d33648f8f8f392a595245bc7bc8426f923ce
Mats Gundersen <matsgundersen@hotmail.com>
2017-05-01 14:39:05 +0200

    footer

--- Prototype/index.php
+++ Prototype/index.php
@@ -77,9 +77,21 @@
 		</div>
 	</div>
 	<!--footer-->
-	<footer class="col-xs-12">
-		<div class="col-md-3 col-md-offset-2"><h2>Kontakt oss</h2><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi nulla non sint in hic tenetur ea ipsam quis vero quia reprehenderit, commodi expedita quibusdam, iste molestias ut libero necessitatibus obcaecati.</p></div>
-		<div class="col-md-3 col-md-offset-2 col-xs-12"><img class="img-responsive" src="http://placehold.it/400x300" alt=""></div>
+	<footer class="col-xs-12 text-center">
+		<h3>KONTAKT OSS</h3>
+		<div><span class="glyphicon glyphicon-phone-alt"></span><p>45 24 81 89</p></div>
+		<div><span class="glyphicon glyphicon-envelope"></span><p>Post@tinde.no</p></div>
+		<div><span class="glyphicon glyphicon-map-marker"></span><p>Frøya 2630 Ringbu</p></div>
+		<br>
+		<div><a href="#">www.tindehytter.no</a></div>
+		<div class="bottom-footer-div">
+			<h4>Våres sosiale medier</h4>
+			<a href="#"><img src="./img/facebook-icon.png " height="25" width="25" ></a>	
+			<a href="#"><img src="./img/twitter-icon.png" height="25" width="25" ></a>
+			<a href="#"><img src="./img/insta-icon.png " height="25" width="25" ></a>
+		</div>
+		
+
 	</footer>
 	</div>
 
